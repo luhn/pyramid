04fe02305a82183f9ed9337c6ad7978605308dcd
Theron Luhn <theron@luhn.com>
2026-03-27 21:07:53 -0700

    Remove test for Python 2.7 regression.

--- tests/test_response.py
+++ tests/test_response.py
@@ -69,30 +69,6 @@ class TestFileResponse(unittest.TestCase):
             )
             r.app_iter.close()
 
-    def test_python_277_bug_15207(self):
-        # python 2.7.7 on windows has a bug where its mimetypes.guess_type
-        # function returns Unicode for the content_type, unlike any previous
-        # version of Python.  See https://github.com/Pylons/pyramid/issues/1360
-        # for more information.
-        import mimetypes as old_mimetypes
-
-        from pyramid import response
-        from pyramid.util import text_
-
-        class FakeMimetypesModule:
-            def guess_type(self, *arg, **kw):
-                return text_('foo/bar'), None
-
-        fake_mimetypes = FakeMimetypesModule()
-        try:
-            response.mimetypes = fake_mimetypes
-            path = self._getPath('xml')
-            r = self._makeOne(path)
-            self.assertEqual(r.content_type, 'foo/bar')
-            self.assertEqual(type(r.content_type), str)
-        finally:
-            response.mimetypes = old_mimetypes
-
 
 class TestFileIter(unittest.TestCase):
     def _makeOne(self, file, block_size):
