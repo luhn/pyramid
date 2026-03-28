8745f722d05ae97f027b36c40f1f820a51b97778
Theron Luhn <theron@luhn.com>
2026-03-27 22:05:01 -0700

    Migrate `pyramid.asset.abspath_from_asset_spec`.

--- src/pyramid/asset.py
+++ src/pyramid/asset.py
@@ -1,7 +1,7 @@
 import os
-import pkg_resources
 
 from pyramid.path import package_name, package_path
+from pyramid.resolver import AssetResolver
 
 
 def resolve_asset_spec(spec, pname='__main__'):
@@ -37,7 +37,4 @@ def asset_spec_from_abspath(abspath, package):
 def abspath_from_asset_spec(spec, pname='__main__'):
     if pname is None:
         return spec
-    pname, filename = resolve_asset_spec(spec, pname)
-    if pname is None:
-        return filename
-    return pkg_resources.resource_filename(pname, filename)
+    return AssetResolver(pname).resolve(spec).abspath()
