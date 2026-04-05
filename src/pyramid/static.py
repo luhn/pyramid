5440eb314a06a3cdded6005a6d781c190b98a938
Theron Luhn <theron@luhn.com>
2026-04-05 05:39:48 +0200

    Refactor `ManifestCacheBuster` to remove `abspath_from_asset_spec`

--- src/pyramid/static.py
+++ src/pyramid/static.py
@@ -2,10 +2,9 @@ from functools import lru_cache
 import json
 import mimetypes
 import os
-from os.path import exists, getmtime, getsize
+from os.path import getmtime, getsize
 import posixpath
 
-from pyramid.asset import abspath_from_asset_spec
 from pyramid.httpexceptions import HTTPMovedPermanently, HTTPNotFound
 from pyramid.path import caller_package
 from pyramid.resolver import AssetResolver
@@ -363,13 +362,12 @@ class ManifestCacheBuster:
     .. versionadded:: 1.6
     """
 
-    exists = staticmethod(exists)  # testing
     getmtime = staticmethod(getmtime)  # testing
 
     def __init__(self, manifest_spec, reload=False):
         package_name = caller_package().__name__
-        self.manifest_path = abspath_from_asset_spec(
-            manifest_spec, package_name
+        self.manifest_asset = AssetResolver(package_name).resolve(
+            manifest_spec
         )
         self.reload = reload
 
@@ -378,13 +376,12 @@ class ManifestCacheBuster:
             self._manifest = self.get_manifest()
 
     def get_manifest(self):
-        with open(self.manifest_path, 'rb') as fp:
+        with self.manifest_asset.stream() as fp:
             return self.parse_manifest(fp.read())
 
     def parse_manifest(self, content):
         """
-        Parse the ``content`` read from the ``manifest_path`` into a
-        dictionary mapping.
+        Parse the ``content`` read from the manifest into a dictionary mapping.
 
         Subclasses may override this method to use something other than
         ``json.loads`` to load any type of file format and return a conforming
@@ -397,9 +394,9 @@ class ManifestCacheBuster:
     def manifest(self):
         """The current manifest dictionary."""
         if self.reload:
-            if not self.exists(self.manifest_path):
+            if not self.manifest_asset.exists():
                 return {}
-            mtime = self.getmtime(self.manifest_path)
+            mtime = self.getmtime(self.manifest_asset.abspath())
             if self._mtime is None or mtime > self._mtime:
                 self._manifest = self.get_manifest()
                 self._mtime = mtime
